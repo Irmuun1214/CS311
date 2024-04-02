81f59e7b698e282d0a19fa7e41139e4f4ee57e96
Bat-Ireedui Irmuun <batirmuun68@gmail.com>
2024-04-02 19:38:38 +0800

    go

--- BagiinAjil2/src/bagiinAjil2/Main2.java
+++ BagiinAjil2/src/bagiinAjil2/Main2.java
@@ -4,10 +4,10 @@ import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.Hashtable;
 
-public class Main {
+public class Main2 {
     public static void main(String[] args) {
-        int ehleh_on = 2006;
-        int duusah_on = 2007;
+        int ehleh_on = 1978;
+        int duusah_on = 1979;
         int butarhai_honog;
         
 
@@ -25,10 +25,10 @@ public class Main {
 
 
         
-        LocalDate[] case2_ehleh_hugatsaa = new LocalDate[16];
-        LocalDate[] case2_duusah_hugatsaa = new LocalDate[16];
-        long[] case2_dund_sur = new long[12]; 
-        long[] case2_ih_sur = new long[4]; 
+        LocalDate[] case1_ehleh_hugatsaa = new LocalDate[16];
+        LocalDate[] case1_duusah_hugatsaa = new LocalDate[16];
+        long[] case1_dund_sur = new long[12]; 
+        long[] case1_ih_sur = new long[4]; 
         int[] buten_doloo_honog = new int[16];
         int[] hicheelleh_honog = new int[16];
 
@@ -37,60 +37,63 @@ public class Main {
         // Anhnii utguudaa zarlav
         
 
-        // Hicheeliin ehleh duusah udur
+        // Hicheeliin ehleh duusah udruus jil bur niit heden honog bgag hagassain butensain orson
         for (int i = 0; i < 16; i++) {
-            case2_ehleh_hugatsaa[i] = LocalDate.of(ehleh_on, 9, 1);
-            case2_duusah_hugatsaa[i] = LocalDate.of(duusah_on, 6, 1);
+            case1_ehleh_hugatsaa[i] = LocalDate.of(ehleh_on, 9, 1);
+            case1_duusah_hugatsaa[i] = LocalDate.of(duusah_on, 6, 1);
             ehleh_on++;
             duusah_on++;
         }
-
-        // Calculate the duration of each academic term
         for (int i = 0; i < 12; i++) {
-            case2_dund_sur[i] = ChronoUnit.DAYS.between(case2_ehleh_hugatsaa[i], case2_duusah_hugatsaa[i]);
+            case1_dund_sur[i] = ChronoUnit.DAYS.between(case1_ehleh_hugatsaa[i], case1_duusah_hugatsaa[i]);
         }
         for (int i = 0; i < 4; i++) {
-            case2_ih_sur[i] = ChronoUnit.DAYS.between(case2_ehleh_hugatsaa[i+12], case2_duusah_hugatsaa[i+12]);
+            case1_ih_sur[i] = ChronoUnit.DAYS.between(case1_ehleh_hugatsaa[i+12], case1_duusah_hugatsaa[i+12]);
         }
 
         
+        
+        
         for (int i = 0; i < 12; i++) {
-            dayOfWeek = case2_ehleh_hugatsaa[i].getDayOfWeek();
+            dayOfWeek = case1_ehleh_hugatsaa[i].getDayOfWeek();
 
-            butarhai_honog = 6-day_of_the_week.get(dayOfWeek);
-            if(day_of_the_week.get(dayOfWeek)>=6){
+            butarhai_honog = 7-day_of_the_week.get(dayOfWeek);
+            if(day_of_the_week.get(dayOfWeek)>6){
                 butarhai_honog =0;
             }
-            dayOfWeek = case2_duusah_hugatsaa[i].getDayOfWeek();
+            dayOfWeek = case1_duusah_hugatsaa[i].getDayOfWeek();
+//            System.out.println(dayOfWeek);
             butarhai_honog = butarhai_honog + 6 -day_of_the_week.get(dayOfWeek);
-            if(day_of_the_week.get(dayOfWeek)==7){
-                butarhai_honog++;
-            }
-           buten_doloo_honog[i] = (int) (case2_dund_sur[i]-butarhai_honog)/7;
-           hicheelleh_honog[i]=(buten_doloo_honog[i]*7)-(buten_doloo_honog[i]*2)+butarhai_honog;
+
+           buten_doloo_honog[i] = (int) (case1_dund_sur[i]-butarhai_honog)/7;
+           
+           hicheelleh_honog[i]=(buten_doloo_honog[i]*7)-(buten_doloo_honog[i]*1)+butarhai_honog;
            butarhai_honog =0;
  
-        }
+        }// Dund surguulid niit heden doloo honog bga eseh
         
         
         for (int i = 0; i < 4; i++) {
-            dayOfWeek = case2_ehleh_hugatsaa[i+12].getDayOfWeek();
+            dayOfWeek = case1_ehleh_hugatsaa[i+12].getDayOfWeek();
 
             butarhai_honog = 6-day_of_the_week.get(dayOfWeek);
             if(day_of_the_week.get(dayOfWeek)>=6){
                 butarhai_honog =0;
             }
-            dayOfWeek = case2_duusah_hugatsaa[i+12].getDayOfWeek();
+            dayOfWeek = case1_duusah_hugatsaa[i+12].getDayOfWeek();
             butarhai_honog = butarhai_honog + 6 -day_of_the_week.get(dayOfWeek);
             if(day_of_the_week.get(dayOfWeek)==7){
                 butarhai_honog++;
             }
-           buten_doloo_honog[i+12] = (int) (case2_ih_sur[i]-butarhai_honog)/7;
+           buten_doloo_honog[i+12] = (int) (case1_ih_sur[i]-butarhai_honog)/7;
 
-           hicheelleh_honog[i+12]=(buten_doloo_honog[i+12]*7)-(buten_doloo_honog[i+12]*2)+butarhai_honog;
+           hicheelleh_honog[i+12]=(buten_doloo_honog[i+12]*7)-(buten_doloo_honog[i+12]*1)+butarhai_honog;
 
            butarhai_honog =0;
-        }
+        }// ih surguulid niit heden doloo honog bga eseh
+        
+        
+        
         System.out.println("============================");
         for (int i = 0; i < 16; i++) {
             if(i<12){
@@ -99,24 +102,34 @@ public class Main {
                 System.out.println("Курс " + (i-11) + " \nДолоо хоног: " +buten_doloo_honog[i]+ " \nНийт өдөр:"+ hicheelleh_honog[i]);
             }
             System.out.println("============================");
-        }
+        }// Dund ba IH surguu;iin medeeleliig hevlene
+        
+        
+        
         long niit_honog=0;
         long niit_hugatsaa;
         long sonirholtoi_tsag=0;
         System.out.println("Дунд сургуулийн цаг");
-        for(int i = 0; i<5; i++){
+        for(int i = 0; i<3; i++){
             niit_honog =niit_honog + hicheelleh_honog[i];
-            sonirholtoi_tsag =sonirholtoi_tsag + buten_doloo_honog[i]*120;
+            sonirholtoi_tsag =sonirholtoi_tsag + buten_doloo_honog[i]*180;
             
-        }
-        niit_hugatsaa = niit_honog*120;
+        }//1-3 angi
+        niit_hugatsaa = niit_honog*45*4;
+        
+        for(int i = 3; i<8; i++){
+            niit_honog =niit_honog + hicheelleh_honog[i];
+            sonirholtoi_tsag =sonirholtoi_tsag + buten_doloo_honog[i]*180;
+            
+        }//4-8 angi
+        niit_hugatsaa = niit_honog*45*34/6;
         
         niit_honog=0;
-        for(int i = 5; i<12; i++){
+        for(int i = 8; i<10; i++){
             niit_honog =niit_honog + hicheelleh_honog[i];
-            sonirholtoi_tsag =sonirholtoi_tsag + buten_doloo_honog[i]*140;
-        }
-        niit_hugatsaa = niit_hugatsaa + niit_honog*210;
+            sonirholtoi_tsag =sonirholtoi_tsag + buten_doloo_honog[i]*180;
+        }//9-10
+        niit_hugatsaa = niit_hugatsaa + niit_honog*45*6;
         System.out.println(niit_hugatsaa/60);
 
         niit_honog=0;
@@ -125,7 +138,7 @@ public class Main {
 
 
         System.out.println("Их сургуулийн цаг");
-        for(int i = 12; i<16; i++){
+        for(int i = 10; i<15; i++){
             niit_honog =niit_honog + hicheelleh_honog[i];
         }
         niit_hugatsaa = niit_honog*270;
