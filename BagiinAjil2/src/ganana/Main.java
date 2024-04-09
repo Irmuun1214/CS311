2e68f14a0f10542e3406bb7907f5464bd811c35a
Bat-Ireedui Irmuun <batirmuun68@gmail.com>
2024-04-09 17:09:18 +0800

    commit

--- BagiinAjil2/src/ganana/Main.java
+++ BagiinAjil2/src/ganana/Main.java
@@ -1,5 +1,6 @@
 package ganana;
 // 2024-04-08 10:31
+// 4:49
 public class Main {
     public static void main(String[] args) {
         Data case1 = new Data();
@@ -14,32 +15,32 @@ public class Main {
         case2.set_angilal_data();
         case2.set_dund_sur_ehleh_on(2006);
 
-//         Call the methods and store the returned values
+
         float dund_sur_sonirholtoi_hugatsaa = case1.get_dund_sur_sonirholtoi_hugatsaa();
         float dund_sur_suraltsah_tsag = case1.get_dund_sur_suraltsah_tsag();
 
-        // Print the calculated values
-        System.out.println("===================================================================================");
-        System.out.println("Dund sur niit buten 7 honog: " + case1.get_dund_sur_buten_doloo_honog());
-        System.out.println("Dund sur niit honog: " + case1.get_dund_sur_hicheellesen_honog());
-        System.out.println("Dund sur sonirholtoi hugatsaa: " + dund_sur_sonirholtoi_hugatsaa+" tsag");
-        System.out.println("Dund sur suraltsah niit tsag: " + dund_sur_suraltsah_tsag+" tsag");
-        
-        System.out.println("Ih sur niit buten 7 honog: " + case1.ih_sur_buten_doloo_honog());
-        System.out.println("Ih sur niit honog: " + case1.ih_sur_hicheellesen_honog());
-        System.out.println("Ih sur suraltsah niit tsag: " + case1.ih_sur_suraltsah_tsag()+" tsag");
-        System.out.println("Ih sur ih_dadlagiin_ezleh_huvi: " + case1.ih_dadlagiin_ezleh_huvi()+" %");
         
         System.out.println("===================================================================================");
-        System.out.println("Dund sur niit buten 7 honog: " + case2.get_dund_sur_buten_doloo_honog());
-        System.out.println("Dund sur niit honog: " + case2.get_dund_sur_hicheellesen_honog());
-        System.out.println("Dund sur sonirholtoi hugatsaa: " + case2.get_dund_sur_sonirholtoi_hugatsaa()+" tsag");
-        System.out.println("Dund sur suraltsah niit tsag: " + case2.get_dund_sur_suraltsah_tsag()+" tsag");
+        System.out.println("Дунд сургуулийн нийт бүтэн 7 хоног: " + case1.get_dund_sur_buten_doloo_honog());
+        System.out.println("Дунд сургуулийн нийт хичээллэсэн хоног: " + case1.get_dund_sur_hicheellesen_honog());
+        System.out.println("Дунд сургуулийн сонирхолтой хичээлд зарцуулсан хугацаа: " + dund_sur_sonirholtoi_hugatsaa+" tsag");
+        System.out.println("Дунд сургуулийн хичээлд зарцуулсан хугацаа: " + dund_sur_suraltsah_tsag+" tsag");
         
-        System.out.println("Ih sur niit buten 7 honog: " + case2.ih_sur_buten_doloo_honog());
-        System.out.println("Ih sur niit honog: " + case2.ih_sur_hicheellesen_honog());
-        System.out.println("Ih sur suraltsah niit tsag: " + case2.ih_sur_suraltsah_tsag()+" tsag");
-        System.out.println("Ih sur ih_dadlagiin_ezleh_huvi: " + case2.ih_dadlagiin_ezleh_huvi()+" %");
+        System.out.println("Их сургуулийн нийт бүтэн 7 хоног: " + case1.ih_sur_buten_doloo_honog());
+        System.out.println("Их сургуулийн нийт хичээллэсэн хоног: " + case1.ih_sur_hicheellesen_honog());
+        System.out.println("Их сургуулийн хичээлд зарцуулсан хугацаа: " + case1.ih_sur_suraltsah_tsag()+" tsag");
+        System.out.println("Их сургуулийн дадлагын эзлэх хувь: " + case1.ih_dadlagiin_ezleh_huvi()+" %");
+        
+        System.out.println("===================================================================================");
+        System.out.println("Дунд сургуулийн нийт бүтэн 7 хоног: " + case2.get_dund_sur_buten_doloo_honog());
+        System.out.println("Дунд сургуулийн нийт хичээллэсэн хоног: " + case2.get_dund_sur_hicheellesen_honog());
+        System.out.println("Дунд сургуулийн сонирхолтой хичээлд зарцуулсан хугацаа: " + case2.get_dund_sur_sonirholtoi_hugatsaa()+" tsag");
+        System.out.println("Дунд сургуулийн хичээлд зарцуулсан хугацаа: " + case2.get_dund_sur_suraltsah_tsag()+" tsag");
+        
+        System.out.println("Их сургуулийн нийт бүтэн 7 хоног: " + case2.ih_sur_buten_doloo_honog());
+        System.out.println("Их сургуулийн нийт хичээллэсэн хоног: " + case2.ih_sur_hicheellesen_honog());
+        System.out.println("Их сургуулийн хичээлд зарцуулсан хугацаа: " + case2.ih_sur_suraltsah_tsag()+" tsag");
+        System.out.println("Их сургуулийн дадлагын эзлэх хувь: " + case2.ih_dadlagiin_ezleh_huvi()+" %");
         System.out.println("===================================================================================");
         
         
